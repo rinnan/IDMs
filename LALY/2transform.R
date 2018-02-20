9ae25528ec6853591a5eaaf5f5efda37014adef1
rinnan <rinnan@u.washington.edu>
2018-02-20 14:22:49 -0800

    Change historical file names.

--- LALY/2transform.R
+++ LALY/2transform.R
@@ -5,8 +5,12 @@ library(grid)
 library(animation)
 library(rgdal)
 
-states <- readOGR("/Users/Darwin/Google Drive/Shapefiles/US_States", "states") #,proj4string=CRS(projection(mat2020)))
-provs <- readOGR("/Users/Darwin/Google Drive/Shapefiles/CAN_Provinces", "PROVINCE") #, proj4string=CRS(projection(mat2020)))
+setwd("~/Google Drive/IDMs/LALY")
+#setwd("\\\\main.sefs.uw.edu\\main\\Space\\Lawler\\Shared\\Scott\\IDMs\\LALY")
+#setwd("H:/IDMs/LALY")
+
+states <- readOGR("Data/US_States", "states") #,proj4string=CRS(projection(mat2020)))
+provs <- readOGR("Data/CAN_Provinces", "PROVINCE") #, proj4string=CRS(projection(mat2020)))
 
 laly <- readOGR("/Users/Darwin/Desktop/Work data/Tree distribution shapefiles", "larilyal")
 crs(laly) <- crs(states)
