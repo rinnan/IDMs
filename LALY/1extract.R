31740fb1c55d74021cf13b1591e69593177cbb4a
rinnan <rinnan@u.washington.edu>
2018-02-20 12:46:21 -0800

    Get climate data.

---
+++ LALY/1extract.R
@@ -0,0 +1,14 @@
+library(raster)
+
+setwd("~/Google Drive/IDMs/LALY")
+#setwd("\\\\main.sefs.uw.edu\\main\\Space\\Lawler\\Shared\\Scott\\IDMs\\LALY")
+#setwd("H:/IDMs/LALY")
+
+# Historical data: download by tile; need tiles 11 and 12
+histmat <- getData("worldclim", var = "bio", res = 0.5, lon = -115, lat = 48, download = T, path = "Data")
+
+
+# Future data: MRI-CGCM3 scenario, RCP6.0 for 2050 and 2070
+mg60bi50 <- getData("CMIP5", var = "bio", res = 0.5, download = T, path = "Data", model = "MG", year = 50, rcp = 60)
+mg60bi70 <- getData("CMIP5", var = "bio", res = 0.5, download = T, path = "Data", model = "MG", year = 70, rcp = 60)
+
