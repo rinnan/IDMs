5dde015e8cf65db37917b0bfa53069de71732349
rinnan <rinnan@u.washington.edu>
2018-02-20 14:37:07 -0800

    Change historical file names.

--- LALY/2transform.R
+++ LALY/2transform.R
@@ -5,8 +5,19 @@ library(grid)
 library(animation)
 library(rgdal)
 
-states <- readOGR("/Users/Darwin/Google Drive/Shapefiles/US_States", "states") #,proj4string=CRS(projection(mat2020)))
-provs <- readOGR("/Users/Darwin/Google Drive/Shapefiles/CAN_Provinces", "PROVINCE") #, proj4string=CRS(projection(mat2020)))
+setwd("~/Google Drive/IDMs/LALY")
+setwd("\\\\main.sefs.uw.edu\\main\\Space\\Lawler\\Shared\\Scott\\IDMs\\LALY")
+setwd("H:/IDMs/LALY")
+
+
+states <- readOGR("Data/US_States", "states") #,proj4string=CRS(projection(mat2020)))
+provs <- readOGR("Data/CAN_Provinces", "PROVINCE") #, proj4string=CRS(projection(mat2020)))
 
 laly <- readOGR("/Users/Darwin/Desktop/Work data/Tree distribution shapefiles", "larilyal")
 crs(laly) <- crs(states)
+
+# rename historical rasters to order them properly; only need to do this once
+hist.files <- list.files("Data/wc0.5", full.names = T)
+hist.files.new <- sub(pattern = "bio([1-9])_", replacement = "bio0\\1_", x = hist.files)
+file.rename(from = hist.files, to = hist.files.new)
+
