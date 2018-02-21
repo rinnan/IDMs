631474cec19f75dde90d17f1e35d1cbb0ac69457
Scott S Rinnan <rinnan@uw.edu>
2018-02-20 16:31:04 -0800

    All directories accessible.

--- LALY/1extract.R
+++ LALY/1extract.R
@@ -1,11 +1,12 @@
 library(raster)
 
 setwd("~/Google Drive/IDMs/LALY")
-#setwd("\\\\main.sefs.uw.edu\\main\\Space\\Lawler\\Shared\\Scott\\IDMs\\LALY")
-#setwd("H:/IDMs/LALY")
+setwd("\\\\main.sefs.uw.edu\\main\\Space\\Lawler\\Shared\\Scott\\IDMs\\LALY")
+setwd("H:/IDMs/LALY")
 
 # Historical data: download by tile; need tiles 11 and 12
 histmat <- getData("worldclim", var = "bio", res = 0.5, lon = -115, lat = 48, download = T, path = "Data")
+histmat11 <- getData("worldclim", var = "bio", res = 0.5, lon = -125, lat = 48, download = T, path = "Data")
 
 
 # Future data: MRI-CGCM3 scenario, RCP6.0 for 2050 and 2070
