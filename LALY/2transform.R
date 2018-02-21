37f9a1aa8c590a4932fabbfb0cbc9fee51bed459
Scott S Rinnan <rinnan@uw.edu>
2018-02-20 16:33:06 -0800

    Created, cropped, and saved bricks.

--- LALY/2transform.R
+++ LALY/2transform.R
@@ -13,7 +13,7 @@ setwd("H:/IDMs/LALY")
 states <- readOGR("Data/US_States", "states") #,proj4string=CRS(projection(mat2020)))
 provs <- readOGR("Data/CAN_Provinces", "PROVINCE") #, proj4string=CRS(projection(mat2020)))
 
-laly <- readOGR("/Users/Darwin/Desktop/Work data/Tree distribution shapefiles", "larilyal")
+laly <- readOGR("Data/larilyal", "larilyal")
 crs(laly) <- crs(states)
 
 # rename historical rasters to order them properly; only need to do this once
@@ -21,3 +21,62 @@ hist.files <- list.files("Data/wc0.5", full.names = T)
 hist.files.new <- sub(pattern = "bio([1-9])_", replacement = "bio0\\1_", x = hist.files)
 file.rename(from = hist.files, to = hist.files.new)
 
+# construct historical brick
+
+hist.files <- list.files("Data/wc0.5", full.names = T, pattern = "11.bil$")
+hist.br11 <- brick(as.list(hist.files))
+
+hist.files <- list.files("Data/wc0.5", full.names = T, pattern = "12.bil$")
+hist.br12 <- brick(as.list(hist.files))
+
+hist.br <- merge(hist.br11, hist.br12)
+names(hist.br) <- c("MAT", "MDR", "ISO", "TS", "HMmax", "CMmin", "TAR", "MTWQ", "MTDQ", "MTHQ", "MTCQ", 
+                    "MAP", "PWM", "PDM", "PS", "PWQ", "PDQ", "PHQ", "PCQ")
+#plot(hist.br[[1]])
+#plot(laly, add = T)
+
+# crop to laly distribution plus buffer region
+ext <- extent(laly) + 10
+#plot(ext,add=T)
+hist.br.crop <- crop(hist.br, ext)
+
+# save raster
+writeRaster(hist.br.crop, filename = "Data/Hist_LALY.grd")
+
+# construct future bricks
+
+mg60bi50.files <- list.files("Data/cmip5/30s", full.names = T, pattern = "mg60bi50\\d+.tif$")
+mg60bi50.files.new <- sub(pattern = "bi50([1-9]).tif", replacement = "bi500\\1.tif", x = mg60bi50.files)
+file.rename(from = mg60bi50.files, to = mg60bi50.files.new)
+
+mg60bi50.files <- list.files("Data/cmip5/30s", full.names = T, pattern = "mg60bi50\\d+.tif$")
+ras <- list()
+for(i in 1:length(mg60bi50.files)){
+  print(i)
+  temp <- raster(mg60bi50.files[i])
+  ras[[i]] <- crop(temp, ext)
+}
+mg60bi50.br <- brick(ras)
+names(mg60bi50.br) <- c("MAT", "MDR", "ISO", "TS", "HMmax", "CMmin", "TAR", "MTWQ", "MTDQ", "MTHQ", "MTCQ", 
+                    "MAP", "PWM", "PDM", "PS", "PWQ", "PDQ", "PHQ", "PCQ")
+
+writeRaster(mg60bi50.br, filename = "Data/mg60bi50_LALY.grd")
+
+
+
+mg60bi70.files <- list.files("Data/cmip5/30s", full.names = T, pattern = "mg60bi70\\d+.tif$")
+mg60bi70.files.new <- sub(pattern = "bi70([1-9]).tif", replacement = "bi700\\1.tif", x = mg60bi70.files)
+file.rename(from = mg60bi70.files, to = mg60bi70.files.new)
+
+mg60bi70.files <- list.files("Data/cmip5/30s", full.names = T, pattern = "mg60bi70\\d+.tif$")
+ras <- list()
+for(i in 1:length(mg60bi70.files)){
+  print(i)
+  temp <- raster(mg60bi70.files[i])
+  ras[[i]] <- crop(temp, ext)
+}
+mg60bi70.br <- brick(ras)
+names(mg60bi70.br) <- c("MAT", "MDR", "ISO", "TS", "HMmax", "CMmin", "TAR", "MTWQ", "MTDQ", "MTHQ", "MTCQ", 
+                        "MAP", "PWM", "PDM", "PS", "PWQ", "PDQ", "PHQ", "PCQ")
+
+writeRaster(mg60bi70.br, filename = "Data/mg60bi70_LALY.grd")
